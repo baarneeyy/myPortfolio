c39cd850e3b6c47d6451ecddfc9ed4dc63ab2a4f
Tomas Vsetecka <tvsetecka@csoc.eu>
2024-09-06 17:15:01 +0200

    added button navbar to new layou page; basic styling next + expandable project tabs;

---
+++ src/lib/components/navContainer.svelte
@@ -0,0 +1,30 @@
+<script>
+    export let jobs;
+    export let projects;
+    export let socials;
+</script>
+
+<main>
+    {#if jobs }
+        <ul class="navContainer">
+            <li>
+                <a href="binaryconfidence.com">QA Lead @ Binary Confidence</a>
+            </li>
+        </ul>
+    {:else if projects}
+        <ul class="navContainer">
+            <li>
+                BottleBuddy
+            </li>
+        </ul>
+    {:else if socials}
+        <ul class="navContainer">
+            <li>
+                <a href="https://github.com/Baarneeyy">github: u/Baarneeyy</a>
+            </li>
+            <li>
+                <a href="mailto:tom.vseteckaa@gmail.com">mail: tom.vseteckaa@gmail.com</a>
+            </li>
+        </ul>
+    {/if}
+</main>
--- src/routes/layout/+page.svelte
+++ src/routes/layout/+page.svelte
@@ -1,3 +1,39 @@
+<script>
+    import NavContainer from "../../lib/components/navContainer.svelte";
+    
+    export let showJobs = false;
+    export let showProjects = false;
+    export let showSocials = false;
+
+    function handleJobs() {
+        showJobs = !showJobs;
+        showProjects = false;
+        showSocials = false;
+        showSomething()
+    }
+
+    function handleProjects() {
+        showProjects = !showProjects;
+        showJobs = false;
+        showSocials = false;
+        showSomething()
+    }
+
+    function handleSocials() {
+        showSocials = !showSocials;
+        showJobs = false;
+        showProjects = false;
+        showSomething()
+    }
+
+    export let something = false;
+
+    export let showSomething = () => {
+        something = showJobs || showProjects || showSocials
+    };
+
+</script>
+
 <main>
     <div class="mainSection controlSide">
         <header>
@@ -10,20 +46,25 @@
                 <p>now what is this</p>
             </div>
         </header>
-        <div>
-            <button>
+        <div class="buttonNav">
+            <button on:click={handleJobs}>
                 <p>my jobs</p>
             </button>
-            <button>
+            <button on:click={handleProjects}>
                 <p>my projects</p>
             </button>
-            <button>
+            <button on:click={handleSocials}>
                 <p>my socials</p>
             </button>
         </div>
+        <p class="check">{something}</p>
     </div>
-    <div class="mainSection">
-        <h1>i <s>(break)</s> make stuff</h1>
+    <div class="mainSection contentSide">
+        {#if something}
+            <NavContainer jobs={showJobs} projects={showProjects} socials={showSocials}/>
+        {:else}
+            <h1>i <s>(break)</s> make stuff</h1>
+        {/if}
     </div>
 </main>
 
@@ -38,10 +79,31 @@
         height: 100vh;
     }
 
+    .controlSide {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-around;
+    }
+
+    .contentSide {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .buttonNav {
+        display: flex;
+        justify-content: center;
+    }
+
     header {
         display: flex;
         align-items: center;
         justify-content: space-around;
+    }
+
+    .check {
+        text-align: center;
     }
 
     
